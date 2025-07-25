a04d45273b56cc779bbf28a4448dd7e24f80e0ed
Alex Baldwin <kalyptein@gmail.com>
2025-07-25 01:36:33 -0500

    more test

---
+++ site/scripts/utils.ts
@@ -0,0 +1,16 @@
+
+// split big string literal into a list
+// declare if this will be indexed by 
+
+// table = makeList(key=false, index='weight | range | undefined', key=false)
+    // index determines if the table includes weight (x-in-sum(x's)), range (x or x-y on the roll) and/or a string key
+    // [weight / range] [key (one word)] description
+        // include error checking for overlaps in the ranges
+    // if no range or weight is included, assume each line has a weight of 1
+
+
+// random choice from list (with or without replacement, so card draw or dice roll)
+
+
+// This is external JavaScript code
+document.getElementById("resultDisplay").textContent = "Content added by external JavaScript!";
