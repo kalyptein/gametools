db42bf4665f2aacdfffcdbfeb5c368e5afb3189e
Alex Baldwin <kalyptein@gmail.com>
2025-07-25 02:08:28 -0500

    utils WIP, hellenica-cities.js as test case

--- site/scripts/utils.js
+++ site/scripts/utils.js
@@ -2,7 +2,7 @@
 // split big string literal into a list
 // declare if this will be indexed by 
 
-// table = makeList(key=false, index='weight | range | undefined', key=false)
+// table = makeList(key=false, index='weight | range | undefined', key=false, content: string)
     // index determines if the table includes weight (x-in-sum(x's)), range (x or x-y on the roll) and/or a string key
     // [weight / range] [key (one word)] description
         // include error checking for overlaps in the ranges
@@ -13,4 +13,18 @@
 
 
 // This is external JavaScript code
-document.getElementById("resultDisplay").textContent = "Content added by external JavaScript!";
+document.getElementById("resultDisplay").textContent = "Content added by external JavaScript!"
+
+/** Parse content to create a table */
+exports.makeTable = (content, index=undefined, key=false) => {
+    table = {
+        content: [],
+    }
+
+    return table
+}
+
+/** Random choice from table */
+exports.pick = (table) => {
+}
+
