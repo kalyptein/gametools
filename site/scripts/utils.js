3017441bec6a9836e69a4e838451689e9c6a22e9
Kalyptein <kalyptein@gmail.com>
2025-08-06 03:28:51 -0500

    makeTable wip

--- site/scripts/utils.js
+++ site/scripts/utils.js
@@ -24,13 +24,25 @@ const RANGE = 'range'
 // This is external JavaScript code
 // document.getElementById("resultDisplay").textContent = "Content added by external JavaScript!"
 
+// regex pieces
+const gap = `\\s*`
+const key = `(\\S+)`
+const wt = `([0-9]+)`
+const range = '(-?[0-9]+)' + gap + ':' + gap + '(-?[0-9]+)'
 
+const regex = {
+    key:    new RegExp(`^${key+gap}(.+)$`),
+    weight: new RegExp(`^${wt+gap}(.+)$`),
+    range:  new RegExp(`^${range+gap}(.+)$`),
+    weightAndKey: new RegExp(`^${wt+gap+key+gap}(.+)$`),
+    rangeAndKey:  new RegExp(`^${range+gap+key+gap}(.+)$`),
+}
 
-const keyRegex =              new RegExp(`^([A-Za-z0-9_\\-]+)\\s+(.+)$`)                                    // textKey rest of line is description                   key the rest is description
-const weightRegex =           new RegExp(`^([0-9]+)\\s+(.+)$`)                                              // weightNumber rest of line is description              3 the rest is description
-const weightAndKeyRegex =     new RegExp(`^([0-9]+)\\s+([A-Za-z0-9_\\-]+)\\s+(.+)$`)                        // weightNumber textKey rest of line is description      3 key the rest is description
-const rangeRegex =            new RegExp(`^(-?[0-9]+)\\s*:\\s*(-?[0-9]+)\\s+(.+)$`)                         // x:y rest of line is description                       2:7 the rest is description
-const rangeAndKeyRangeRegex = new RegExp(`^(-?[0-9]+)\\s*:\\s*(-?[0-9]+)\\s+([A-Za-z0-9_\\-]+)\\s+(.+)$`)   // x-y textKey rest of line is description               2:7 key the rest is description
+// const keyRegex =              new RegExp(`^([A-Za-z0-9_\\-]+)\\s+(.+)$`)                                    // textKey rest of line is description                   key the rest is description
+// const weightRegex =           new RegExp(`^([0-9]+)\\s+(.+)$`)                                              // weightNumber rest of line is description              3 the rest is description
+// const weightAndKeyRegex =     new RegExp(`^([0-9]+)\\s+([A-Za-z0-9_\\-]+)\\s+(.+)$`)                        // weightNumber textKey rest of line is description      3 key the rest is description
+// const rangeRegex =            new RegExp(`^(-?[0-9]+)\\s*:\\s*(-?[0-9]+)\\s+(.+)$`)                         // x:y rest of line is description                       2:7 the rest is description
+// const rangeAndKeyRangeRegex = new RegExp(`^(-?[0-9]+)\\s*:\\s*(-?[0-9]+)\\s+([A-Za-z0-9_\\-]+)\\s+(.+)$`)   // x-y textKey rest of line is description               2:7 key the rest is description
 
 /** Parse content to create a table (index = 'weight' | 'range' | undefined) */
 exports.makeTable = (content, name="new-table", index=undefined, keyed=false) => {
@@ -39,47 +51,76 @@ exports.makeTable = (content, name="new-table", index=undefined, keyed=false) =>
         name: name,
         content: undefined,
         index: undefined,
-        keys: undefined
+        keys: {}
     }
 
+    let re = undefined
     switch (index) {
-        case WEIGHT: table.index = WEIGHT; break;
-        case RANGE: table.index = RANGE; break;
+        case WEIGHT:
+            table.index = WEIGHT;
+            re = (keyed) ? regex.weightAndKey : regex.weight
+            break;
+        case RANGE: 
+            table.index = RANGE;
+            re = (keyed) ? regex.rangeAndKey : regex.range
+            break;
+        default:
+            table.index = WEIGHT;
+            re = (keyed) ? regex.key : undefined
+            break;
     }
 
-    // split lines, trim whitespace, remove empty lines
-    content = content.split('\n')
-        .map(line => line.trim())
-        .filter(line => line)
-        .map(line => { 
-            let x = { key: undefined, index: 1, description: line };
-            return x;
+    // split lines, trim whitespace, remove empty lines, parse w/ regex
+    let totalWeight = 0;
+    table.content = content.split('\n')
+        .map(line => {
+            line = line.trim()
+            if (!line) { return undefined }
+
+            let parsed = (re) ? line.match(re) : [ '', line ]
+            if (parsed.length < 2) {
+                console.warn(`Line didn't parse w/ regex: ${line}`)
+                return undefined;
+            } else {
+                parsed = { key: '', index: 1, description: '' }
+                parsed.description = parsed[parsed.length-1]
+                switch (index) {
+                    case WEIGHT: 
+                        parsed.index = number(parsed[1])
+                        totalWeight += parsed.index
+                        parsed.key = (keyed) ? parsed[2] : ''
+                        break
+                    case RANGE: 
+                        parsed.index = [ number(parsed[1]), number(parsed[2]) ]
+                        parsed.key = (keyed) ? parsed[3] : ''
+                        break
+                    default: 
+                        parsed.index = 1
+                        totalWeight += parsed.index
+                        parsed.key = (keyed) ? parsed[1] : ''
+                        break
+                }
+                if (parsed.key) {
+                    if (table.keys[parsed.key]) { console.warn(`Duplicate key ${parsed.key} in ${table.name}`) }
+                    table.keys[parsed.key] = parsed
+                }
+            }
+            return parsed
         })
+        .filter(line => line)
 
-    if (keyed && !(table.index)) {
-        keyRegex
-    } else if (!keyed && table.index) {
-        if (table.index == WEIGHT) {
-            weightRegex
-        } else if (table.index == WEIGHT) {
-            rangeRegex
-        }
-    } else if (keyed & table.index) {
-        if (table.index == WEIGHT) {
-            weightAndKeyRegex
-        } else if (table.index == WEIGHT) {
-            rangeAndKeyRangeRegex
-        }
-    } else {
-        // all weights are 1, no keys
-        table.index = WEIGHT
-        table.content = content
+    switch (table.index) {
+        case WEIGHT: 
+            table.totalWeight = totalWeight
+            // TODO turn weights into ranges?
+            break
+        case RANGE:
+            // TODO check for overlapping ranges
+            break
     }
-
-    // log to console the existence of any lines that don't successfully regex, but keep going
 
     if (exports.tables[name]) {
-        console.log(`Table '${name}' already exists.  Overwriting...`)
+        console.warn(`Duplicate table '${name}', overwriting...`)
     }
     exports.tables[name] = table
 
